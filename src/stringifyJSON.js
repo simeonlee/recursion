d6a53b3f0fc574b9861f5e47a51fa50e6389d363
Simeon Lee <simeonhlee@gmail.com>
2016-07-20 15:16:49 -0700

    Add stringifyJSON function definition so that all tests for stringifyJSONSpec pass

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,43 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  var res = '';
+  if (typeof obj === 'number') {
+		res += obj;
+  } else if (obj === null) {
+  	res += null;
+  } else if (obj === true) {
+  	res += true;
+  } else if (obj === false) {
+  	res += false;
+  } else if (typeof obj === 'string') {
+  	res = '"' + obj + '"';
+  } else if (Array.isArray(obj)) {
+  	res += '[';
+  	for (var i = 0; i < obj.length; i++) {
+  		var element = stringifyJSON(obj[i]);
+  		if (i === 0) {
+  			res += element;
+  		} else {
+  			res += ',' + element;
+  		}
+  	}
+  	res += ']';
+  } else if (typeof obj === 'object' && obj.constructor === Object) {
+  	res += '{';
+  	var keys = Object.keys(obj);
+  	for (var j = 0; j < keys.length; j++) {
+  		var key = keys[j];
+  		var val = obj[key];
+  		if ((typeof val === 'function') || (val === undefined)) {
+  			continue;
+  		} else if (j === 0) {
+  			res += stringifyJSON(key) + ':' + stringifyJSON(val);
+  		} else {
+  			res += ',' + stringifyJSON(key) + ':' + stringifyJSON(val);
+  		}
+  	}
+  	res += '}';
+  }
+  return res;
 };
