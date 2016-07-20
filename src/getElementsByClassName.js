c363910b6b0076645747937fd1e1650d618059de
Simeon Lee <simeonhlee@gmail.com>
2016-07-20 16:45:23 -0700

    Clean up comment in getElementByClassName.js

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -1,6 +1,6 @@
 // If life was easy, we could just do things the easy way:
-// var getElementsByclass_name = function (class_name) {
-//   return document.getElementsByclass_name(class_name);
+// var getElementsByClassName = function(class_name) {
+//   return document.getElementsByClassName(class_name);
 // };
 
 // But instead we're going to implement it from scratch:
